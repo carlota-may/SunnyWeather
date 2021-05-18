9e5193892fb42a00fde7dbbb03f1654d993860c1
carlota-may <1006932744@qq.com>
2021-05-18 21:48:21 +0800

    search place

---
+++ app/src/main/java/com/zxm/sunnyweather/ui/place/PlaceAdapter.kt
@@ -0,0 +1,31 @@
+package com.zxm.sunnyweather.ui.place
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.zxm.sunnyweather.R
+import com.zxm.sunnyweather.logic.model.Place
+
+class PlaceAdapter(val fragment: Fragment, val placeList: List<Place>) :
+        RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val placeName: TextView = view.findViewById(R.id.placeName)
+        val placeAddress: TextView = view.findViewById(R.id.placeAddress)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.palce_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val place = placeList[position]
+        holder.placeName.text = place.name
+        holder.placeAddress.text = place.formattedAddress
+    }
+
+    override fun getItemCount() = placeList.size
+}
