8112918d3a4d7e64888f1ee1bbd77b700375878a
carlota-may <1006932744@qq.com>
2021-05-20 21:58:02 +0800

    debug

--- app/src/main/java/com/zxm/sunnyweather/MainActivity.kt
+++ app/src/main/java/com/zxm/sunnyweather/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.zxm.sunnyweather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //TODO 优化 透明状态栏
+        val decorView = window.decorView
+        decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.statusBarColor = resources.getColor(R.color.teal_700)
+
     }
 }
